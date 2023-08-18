3c3a21e9ba416691f687ce2388056bb39242205e
Carlos Mondaca <carlos.a.mondaca.paredes@gmail.com>
2023-08-18 19:51:28 -0400

    v1.0

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Agregamos una clase 'active' al elemento de menú actualmente seleccionado
+const menuItems = document.querySelectorAll('.menu a');
+menuItems.forEach(item => {
+    item.addEventListener('click', function() {
+        menuItems.forEach(i => i.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
+
+// Animación de desplazamiento suave al hacer clic en enlaces del menú
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
